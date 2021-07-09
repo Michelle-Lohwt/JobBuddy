f756b7cd7d0ad78df261e359198d9b963b296d71
Lee Ren Jie <work.renjie@gmail.com>
2021-07-09 22:39:19 +0800

    feat(main.js): bot sends message to channel if pinged

--- main.js
+++ main.js
@@ -2,8 +2,26 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.once('ready', () => {
-  console.log('JobBuddy is online!')
-})
+//Prefix for command in discord
+const prefix = '?';
 
+//Bot status
+client.once('ready', () => {
+  console.log('JobBuddy is working!');
+});
+
+
+client.on('message', message => {
+  // validation
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping'){
+    message.channel.send('Testinggg, I work!!!');
+  }
+});
+
+//This line should always be the last
 client.login('insert_bot_token_here')
