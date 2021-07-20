a24270b3a7066f59eb87ec3ccbf7761043b642e8
Lee Ren Jie <work.renjie@gmail.com>
2021-07-20 22:20:53 +0800

    refactor(main.js): add comment for code

--- main.js
+++ main.js
@@ -2,26 +2,28 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-//Prefix for command in discord
+//prefix for command in discord
 const prefix = '?';
 
-//Bot status
+//Check if the bot is running in console
 client.once('ready', () => {
   console.log('JobBuddy is working!');
 });
 
 
 client.on('message', message => {
-  // validation
+  // validation to do nothing if command does not start with prefix or message is from bot
   if(!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(" ");
   const command = args.shift().toLowerCase();
 
+  //Check if command is valid
   if (command === 'ping'){
     message.channel.send('Testinggg, I work!!!');
   }
 });
 
 //This line should always be the last
+//insert bot's token
 client.login('insert_bot_token_here')
