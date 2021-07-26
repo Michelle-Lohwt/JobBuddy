d961ce926def0d93303b3e950d06613adebc77b4
Lee Ren Jie <work.renjie@gmail.com>
2021-07-26 22:32:33 +0800

    feat(embed): add function to send embedded message with ?embed commmand

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ client.once('ready', () => {
   console.log('JobBuddy is working!');
 });
 
-
 client.on('message', message => {
+  //log messages from users
+  console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
   // validation to do nothing if command does not start with prefix or message is from bot
   if(!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -21,6 +22,30 @@ client.on('message', message => {
   //Check if command is valid
   if (command === 'ping'){
     message.channel.send('Testinggg, I work!!!');
+  }
+  else if (command === 'embed'){
+    //Embed for job posting
+    //Website: https://discordjs.guide/popular-topics/embeds.html
+    const botEmbed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('JobBuddy helps with your job posting \:briefcase:')
+    .setDescription('Have a job opening? React with "\:briefcase:" under this message!')
+    .addFields(
+      //Spacing
+      { name: '\u200B', value: '\u200B' },
+      {
+        name: 'Required information for job posting before reacting to this message',
+        value: 'The bot will ask for:\n\:one: Employment form (Full-time/Part-time/Internship) \n\:two: Salary range \n\:three: Job location \n\:four: Job Work Scope \n\:five: Job Requirements \n\nJob Buddy will reach out via direct message in Discord!'
+      },
+      { name: '\u200B', value: '\u200B' },
+      {
+        name: "What's next?",
+        value: 'If an interested candidate reacts to the job posting, you will receive their information via direct message with JobBuddy again. You can then proceed with the next steps from there.'
+      },
+    )
+    .setTimestamp()
+    .setFooter('JobBuddy Bot by the community, for the community');
+    message.channel.send(botEmbed);
   }
 });
 
